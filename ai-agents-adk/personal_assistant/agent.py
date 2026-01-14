2dba67d9c7c517c836b857ec8bf5b16553e1ae2f
Dhiraj Patra <dhiraj.patra@gmail.com>
2026-01-14 20:00:05 +0530

    updated

--- ai-agents-adk/personal_assistant/agent.py
+++ ai-agents-adk/personal_assistant/agent.py
@@ -1,20 +1,29 @@
+import os
+from dotenv import load_dotenv
 from google.adk.agents import Agent
 from google.adk.tools import FunctionTool
 from google.adk.tools.agent_tool import AgentTool
 from google.adk.tools.langchain_tool import LangchainTool
 
+# --- Load .env values ---
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_AI_STUDIO_API_KEY")
+
 from .custom_functions import get_fx_rate
 from .custom_agents import google_search_agent
 from .third_party_tools import langchain_wikipedia_tool
 
+# Ensure you use the exact string for the model as per Vertex/AI Studio docs
+model = os.getenv("GEMINI_MODEL", "gemini-2.5-flash-lite")
 
 root_agent = Agent(
-    model='gemini-2.5-flash',
-    name='root_agent',
-    description='A helpful assistant for user questions.',
+    model=model,
+    name="root_agent",
+    description="A helpful assistant for user questions.",
     tools=[
-        FunctionTool(get_fx_rate), 
+        FunctionTool(get_fx_rate),
         AgentTool(agent=google_search_agent),
         LangchainTool(langchain_wikipedia_tool),
-    ]
+    ],
 )
--- ai-agents-adk/personal_assistant/custom_agents.py
+++ ai-agents-adk/personal_assistant/custom_agents.py
@@ -1,12 +1,19 @@
 from google.adk.agents import Agent
 from google.adk.tools import google_search
+import os
+from dotenv import load_dotenv
+
+# --- Load .env values ---
+load_dotenv()
+
+model = os.getenv("GEMINI_MODEL", "gemini-2.5-flash-lite")
 
 
 # Create an agent with google search tool as a search specialist
 google_search_agent = Agent(
-    model='gemini-2.5-flash',
-    name='google_search_agent',
-    description='A search agent that uses google search to get latest information about current events, weather, or business hours.',
-    instruction='Use google search to answer user questions about real-time, logistical information.',
+    model=model,
+    name="google_search_agent",
+    description="A search agent that uses google search to get latest information about current events, weather, or business hours.",
+    instruction="Use google search to answer user questions about real-time, logistical information.",
     tools=[google_search],
 )
