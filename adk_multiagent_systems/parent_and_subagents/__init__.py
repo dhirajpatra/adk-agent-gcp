ca29e93415bcecf21cddc9fd9d23b1780c882339
Dhiraj Patra <dhiraj.patra@gmail.com>
2026-01-14 15:23:51 +0530

    updated

--- adk_multiagent_systems/parent_and_subagents/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Initializes the 'parent_and_subagents' Python package.
-
-This file makes Python treat the directory as a module and
-imports the agent definitions from agent.py.
-"""
-from . import agent
--- adk_multiagent_systems/workflow_agents/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Initializes the 'workflow_agents' Python package.
-
-This file makes Python treat the directory as a module and
-imports the agent definitions from agent.py.
-"""
-from . import agent
